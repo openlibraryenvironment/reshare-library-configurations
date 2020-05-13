e34c92d9a4d42b2dc0772057b627ccea57ab3b05
Niels Erik G. Nielsen <nielserik@indexdata.com>
2020-05-14 01:28:13 +0200

    Organize, streamline harvest config scripts for East/West demo

--- east-west/harvesting/configure-storage-localhost.sh
+++
@@ -1,4 +0,0 @@
-echo ========================================
-echo Configure Inventory storage
-echo ========================================
-curl -H "Content-Type: application/xml"  -d @201101-storage-localhost.xml http://localhost:8080/harvester/records/storages
