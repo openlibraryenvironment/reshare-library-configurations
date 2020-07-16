31adfaff6090211d014f118b0c37dc2afb38af27
Niels Erik G. Nielsen <nielserik@indexdata.com>
2020-07-16 18:32:25 +0200

    Tweak script for wiping out inventory records

--- delete-folio-sample-locations/delete-all-items-holdings-instances.sh
+++ delete-folio-sample-locations/delete-all-items-holdings-instances.sh
@@ -1,7 +1,6 @@
-tenant=$1
-token=$2
-protocol=$3
-host=$4
+banner="This will delete all instances with their holdings and items"
+
+. $1
 
 curl -H "X-Okapi-Tenant: $tenant" -H "X-Okapi-Token: $token" -X DELETE $protocol://$host/item-storage/items
 curl -H "X-Okapi-Tenant: $tenant" -H "X-Okapi-Token: $token" -X DELETE $protocol://$host/holdings-storage/holdings
