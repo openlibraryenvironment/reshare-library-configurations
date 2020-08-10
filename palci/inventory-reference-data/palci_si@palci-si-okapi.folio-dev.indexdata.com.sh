86e67bf7faff9f4113c983081532ffd6db4b4d63
Niels Erik G. Nielsen <nielserik@indexdata.com>
2020-08-10 22:34:12 +0200

    Add FOLIO Inventory config for PALCI SI

--- palci/inventory-reference-data/palci_si@palci-si-okapi.folio-dev.indexdata.com.sh
+++ palci/inventory-reference-data/palci_si@palci-si-okapi.folio-dev.indexdata.com.sh
@@ -1,4 +1,4 @@
-# Source this file in scripts to run them against tenant diku on localhost (username diku_admin)
+# Source this file in scripts to run them against tenant palci_si on palci-si-okapi.folio-dev.indexdata.com (username palci_admin)
 host=palci-si-okapi.folio-dev.indexdata.com
 protocol=https
 tenant=palci_si
