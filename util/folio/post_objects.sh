ba76b5f3d31468102e85f65e755602ecf3482e02
Charles Ledvina <charles@indexdata.com>
2020-12-16 12:55:20 -0600

    Load a glob of object files.

--- util/folio/post_objects.sh
+++ util/folio/post_objects.sh
@@ -1,43 +1,44 @@
 #!/bin/bash
 
-UFILE=$1;
-if [ -z $UFILE ]
-  then
-    echo "Usage: . ${BASH_SOURCE[0]} <jsonl_file>"
-    return
-fi
-if [ ! -f $UFILE ]
-  then
-    echo 'File not found'
-    return
-fi
-if [ -z $token ]
-  then
-    echo "Can't find okapi token-- Make sure to run a login script."
-    return 
-fi
-OPTS=`cat endpoints`;
+for UFILE in $@; do
+  if [ -z $UFILE ]
+    then
+      echo "Usage: . ${BASH_SOURCE[0]} <jsonl_file>"
+      return
+  fi
+  if [ ! -f $UFILE ]
+    then
+      echo 'File not found'
+      return
+  fi
+  if [ -z $token ]
+    then
+      echo "Can't find okapi token-- Make sure to run a login script."
+      return 
+  fi
+  OPTS=`cat endpoints`;
 
-case $UFILE in
-  *identifier-types.jsonl) EP=identifier-types;; 
-  *service-points.jsonl) EP=service-points;;
-  *institutions.jsonl) EP=location-units/institutions;;
-  *campuses.jsonl) EP=location-units/campuses;;
-  *libraries.jsonl) EP=location-units/libraries;;
-  *locations.jsonl) EP=locations;;
-  *)
-    echo "Choose an endpoint..."
-    select EP in $OPTS 
+  case $UFILE in
+    *identifier-types.jsonl) EP=identifier-types;; 
+    *service-points.jsonl) EP=service-points;;
+    *institutions.jsonl) EP=location-units/institutions;;
+    *campuses.jsonl) EP=location-units/campuses;;
+    *libraries.jsonl) EP=location-units/libraries;;
+    *locations.jsonl) EP=locations;;
+    *)
+      echo "Choose an endpoint..."
+      select EP in $OPTS 
+      do
+        break
+      done
+    ;;
+  esac
+
+  LN=1;
+  while IFS= read -r line
     do
-      break
-    done
-  ;;
-esac
-
-LN=1;
-while IFS= read -r line
-  do
-    echo "Loading # ${LN}"
-    curl -w '\n' --http1.1 "$protocol://$host/${EP}" -H 'content-type: application/json' -H "x-okapi-tenant: $tenant" -H "x-okapi-token: $token" -d "${line}"
-    LN=$(expr $LN + 1)
-done < $UFILE
+      echo "Loading Record# ${LN} from ${UFILE}"
+      curl -w '\n' --http1.1 "$protocol://$host/${EP}" -H 'content-type: application/json' -H "x-okapi-tenant: $tenant" -H "x-okapi-token: $token" -d "${line}"
+      LN=$(expr $LN + 1)
+  done < $UFILE
+done
