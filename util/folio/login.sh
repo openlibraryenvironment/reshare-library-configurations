b10fd8dcd616f8f32f04082d6c0a47b27fceee9a
Charles Ledvina <charles@indexdata.com>
2021-01-04 11:46:39 -0600

    Run return or exit command when login is cancelled.

--- util/folio/login.sh
+++ util/folio/login.sh
@@ -11,7 +11,7 @@ if [ -z $password ]; then
   echo
   echo " Cancelled"
   echo
-  exit 1
+  return || exit 1
 fi
 
 token=$(curl -s -X POST -D - -H "Content-type: application/json" -H "X-Okapi-Tenant: $tenant"  -d "{ \"username\": \"$username\", \"password\": \"$password\"}" "$protocol://$host/authn/login" | grep x-okapi-token | tr -d '\r' | cut -d " " -f2)
@@ -21,6 +21,6 @@ if [ -z $token ]; then
   echo
   echo " Exiting. Could not authenticate (get token) for user [$username]"
   echo
-  exit 1
+  return || exit 1
 fi
 echo
