225fc59e2ac84957f2c1f56a9cb35465a161f86d
Niels Erik G. Nielsen <nielserik@indexdata.com>
2021-02-20 15:18:24 +0100

    Put FOLIO login scripts in /util/folio/

---
+++ util/folio/palci_si@palci-si-okapi.folio-dev.indexdata.com.sh
@@ -0,0 +1,7 @@
+# Source this file in scripts to run them against tenant palci_si on palci-si-okapi.folio-dev.indexdata.com (username palci_admin)
+host=palci-si-okapi.folio-dev.indexdata.com
+protocol=https
+tenant=palci_si
+username=palci_admin
+
+. login.sh
---
+++ util/folio/si_dev@si-dev-okapi.folio-dev.indexdata.com.sh
@@ -0,0 +1,7 @@
+# Source this file in scripts to run them against tenant si_dev on si-dev-okapi.folio-dev.indexdata.com (username si_admin)
+host=si-dev-okapi.folio-dev.indexdata.com
+protocol=https
+tenant=si_dev
+username=si_admin
+
+. login.sh
