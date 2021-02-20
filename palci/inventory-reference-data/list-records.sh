9c20fcdea3cbe9e212fdab6158a13f5ef8a51cce
Niels Erik G. Nielsen <nielserik@indexdata.com>
2021-02-20 09:05:58 +0100

    Remove old list-records.sh

--- palci/inventory-reference-data/list-records.sh
+++
@@ -1,5 +0,0 @@
-banner="Listing records from [$2]"
-
-. $1
-
-curl $protocol://$host/$2?limit=999 -H "X-Okapi-Tenant: $tenant" -H "X-Okapi-Token: $token" -H "Content-Type: application/json" -H "Accept: application/json"  
