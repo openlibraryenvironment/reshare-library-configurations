97163a122215ccdbdb7f9013f1b2d78758c0f9b5
Charles Ledvina <charles@indexdata.com>
2021-03-15 13:15:32 -0500

    New login and ref data for CNY

---
+++ util/folio/cny_si@cny-okapi.reshare.indexdata.com.sh
@@ -0,0 +1,7 @@
+# Source this file in scripts to run them against tenant cny_si on cny-okapi.reshare.indexdata.com (username cny_admin)
+host=cny-okapi.reshare.indexdata.com
+protocol=https
+tenant=cny_si
+username=cny_admin
+
+. login.sh
