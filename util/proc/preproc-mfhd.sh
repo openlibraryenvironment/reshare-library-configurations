f6aa03dc3e10381e98b7477e78720718c16a342d
Charles Ledvina <charles@indexdata.com>
2021-10-14 16:05:34 -0500

    Bash script for processing MFHD merging.

---
+++ util/proc/preproc-mfhd.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ $1 ] 
+then
+	DAYS=$1
+else
+	DAYS=1
+fi
+
+ACCS='fandm moravian pcom robertmorris shu wvu westminster'
+
+for a in $ACCS
+do      
+	LOGFILE=$a.log
+	rm -f $LOGFILE
+	FPATH=/home/$a/ftp/incoming
+	MRCFILES=$(find $FPATH/*.* -maxdepth 0 -mtime -$DAYS)
+	for mf in $MRCFILES
+	do
+		MCOM="./merge_mfhd.pl $mf"
+		echo "Running ${MCOM}";
+		$MCOM >> $LOGFILE
+	done
+done
