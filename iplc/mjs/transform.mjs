33b5ad654a03e11f1d9b9547c412c5f91283f187
Charles Ledvina <charles@indexdata.com>
2022-06-23 10:39:41 -0500

    Merge/dedupe marc records

--- iplc/mjs/transform.mjs
+++ iplc/mjs/transform.mjs
@@ -30,13 +30,14 @@ function getSubs(field) {
   return subs;
 }
 
-export function cluster_transform(clusterStr) {
+function cluster_transform_old(clusterStr) {
+  
   let cluster = JSON.parse(clusterStr);
   cluster.records.forEach(r => {
     let sid = r.sourceId;
     let lid = r.localId;
-    let rec = (r.payload) ? r.payload.marc : '';
-    let matType = rec.leader.substring(6,8);
+    let rec = r.payload.marc;
+    let matType = rec.leader.substring(6, 8);
     let fields = getFields(rec);
     let locFields = localFields[sid];
     let itemTag = (locFields) ? locFields.item.tag : '';
@@ -85,3 +86,49 @@ export function cluster_transform(clusterStr) {
   });
   return JSON.stringify(cluster, null, 2);
 }
+
+export function cluster_transform(clusterStr) {
+  let now = new Date().toISOString();
+  now = now.replace(/^(\d{4})-(\d\d)-(\d\d).*/, '$1$2$3000000.0')
+  const f005 = now;
+  let cluster = JSON.parse(clusterStr);
+  let crecs = cluster.records;
+  const out = {}
+  let fields = [];
+  const f001 = cluster.clusterId;
+  let f008 = '';
+  let tiSeen = 0;
+  for (let x = 0; x < crecs.length; x++) {
+    let crec = crecs[x];
+    let rec = crec.payload.marc;
+    out.leader = rec.leader;
+    for (let y = 0; y < rec.fields.length; y++) {
+      let field = rec.fields[y];
+      let tag = Object.keys(field)[0];
+      if (tag > '009' && tag < '831') {
+        if (tag !== '245' || tag === '245' && !tiSeen) {
+          let fkey = JSON.stringify(field);
+          fields.push(fkey);
+        }
+        if (tag === '245') tiSeen = 1;
+      }
+      if (tag === '008') f008 = field['008'];
+    }
+  }
+  fields.sort();
+  let preKey = '';
+  out.fields = [];
+  for (let z = 0; z < fields.length; z++) {
+    let fkey = fields[z];
+    if (fkey != preKey) {
+      let fobj = JSON.parse(fkey);
+      out.fields.push(fobj);
+    }
+    preKey = fkey;
+  }
+  out.fields.unshift({ '008': f008 });
+  out.fields.unshift({ '005': f005 });
+  out.fields.unshift({ '001': f001 });
+  return JSON.stringify(out, null, 2);
+}
+
