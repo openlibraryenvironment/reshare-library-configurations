51a7825b49602a53f33f77aca5a62c6ed6bc47c7
Charles Ledvina <charles@indexdata.com>
2022-11-30 16:40:58 -0600

    A couple more helper scripts.

---
+++ palci/mjs/okapi_scripts/get-cluster-source.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+TMP='./.okapi'
+OKAPI=`cat ${TMP}/url`
+TOKEN=`cat ${TMP}/token | sed 's/.$//'`
+
+SRCID=$1
+
+if [ -z $SRCID ]
+then
+  echo "Usage: ${0} <sourceId>"
+  exit
+fi
+
+URL="${OKAPI}/reservoir/clusters?matchkeyid=goldrush&query=sourceId==${SRCID}"
+
+curl --http1.1 -w '\n' -s $URL -H "x-okapi-token: ${TOKEN}"
