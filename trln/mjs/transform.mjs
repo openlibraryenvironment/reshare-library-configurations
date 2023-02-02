ac22a7ff58cf100df1f4f9bbee65c4af2ebc4907
Charles Ledvina <charles@indexdata.com>
2023-02-02 14:08:18 -0600

    Support indicators.

--- trln/mjs/transform.mjs
+++ trln/mjs/transform.mjs
@@ -7,7 +7,7 @@ const localFields = {
   'US-NCD': {
     name: 'Duke University',
     tag: '940',
-    subs: { a: 'b,c', b: 'p', c: 'h', d: 'd', x: 'r', y: 'k', s: 'm', z: 'z' },
+    subs: { a: 'b,c', b: 'p', c: 'h', d: 'd', x: 'r', y: 'k', m: 'n', z: 'z' },
     lendLocs: ['LAW LALUI', 'LAW LCHRI', 'LAW LCOX', 'LAW LEMP', 'LAW LGEN', 'LILLY PL2', 'LILLY PL2F', 'LILLY PL3', 'LILLY PLD', 'LILLY PLI', 'LILLY PLW', 'LILLY PLWF', 'LSC PSB', 'LSC PSBF', 'LSC PSBQ', 'LSC PSD', 'LSC PSDF', 'LSC PSDQ', 'LSC PSDV', 'LSC PSK', 'LSC PSK2', 'LSC PSK6', 'LSC PSKC', 'LSC PSKE', 'LSC PSKEF', 'LSC PSKEQ', 'LSC PSKF', 'LSC PSKJ', 'LSC PSKL', 'LSC PSKL', 'LSC PSKLF', 'LSC PSKN', 'LSC PSKOH', 'LSC PSKQ', 'LSC PSV', 'LSC PSZ', 'MUSIC PM', 'MUSIC PM9', 'MUSIC PMF', 'MUSIC PMF9', 'MUSIC PMMS', 'MUSIC PMMS9', 'MUSIC PMT', 'MUSIC PMTF', 'PERKN PK', 'PERKN PK2', 'PERKN PKE', 'PERKN PKEF', 'PERKN PKF', 'PERKN PKFF', 'PERKN PKQ', 'PERKN PKT']
   },
   'US-NCRS': {
@@ -15,7 +15,14 @@ const localFields = {
     tag: '999',
     subs: { a: 'm,l', c: 'a', d: 'w', x: 't', y: 'i' },
     lendLocs: ['BOOKBOT CUR-PER', 'BOOKBOT FLOATDVD', 'BOOKBOT HAB-REF', 'BOOKBOT MATH', 'BOOKBOT MEDIA', 'BOOKBOT MFGUIDES', 'BOOKBOT MICROFORMS', 'BOOKBOT NCDOCS-MF', 'BOOKBOT OVERSIZE', 'BOOKBOT OVERSIZE2', 'BOOKBOT REF', 'BOOKBOT REF-USDOCS', 'BOOKBOT SMALLBOOK', 'BOOKBOT STACKS', 'BOOKBOT STCKS-SRL', 'BOOKBOT US-DOCS', 'BOOKBOT US-DOCS-CB', 'BOOKBOT US-DOCS-MF', 'BOOKBOT US-DOCS-MP', 'BOOKBOT US-DOCS-O2', 'BOOKBOT US-DOCS-OV', 'BOOKBOT US-DOCS-PF', 'BOOKBOT US-DOCS-PT', 'DESIGN OVERSIZE', 'DESIGN OVERSIZE2', 'DESIGN SMALLBOOK', 'DESIGN STACKS', 'DHHILL DATASPACE', 'DHHILL DISPLAY', 'DHHILL EXHIBITS', 'DHHILL FACPUBS-RR', 'DHHILL FACPUBS-RR', 'DHHILL FLOATDVD', 'DHHILL INNOSTUD', 'DHHILL JUV-FICT', 'DHHILL MAKERSPACE', 'DHHILL MAPS', 'DHHILL MICROFORMS', 'DHHILL NCDOCS-MF', 'DHHILL NEWBOOK', 'DHHILL OVERSIZE', 'DHHILL OVERSIZE2', 'DHHILL POPREAD', 'DHHILL SMALLBOOK', 'DHHILL STACKS', 'DHHILL STCKS-SRL', 'DHHILL TECCLASSIC', 'DHHILL US-DOCS', 'DHHILL US-DOCS-MF', 'DHHILL US-DOCS-PF', 'DHHILL US-DOCS-PT', 'HUNT DATASPACE', 'HUNT FACPUBS-RR', 'HUNT FLOATDVD', 'HUNT OVERSIZE', 'HUNT SCI-BEST', 'HUNT STACKS', 'HUNT TECCLASSIC', 'LRL GRAPH-NOV', 'LRL INFOTEXT', 'LRL JUV-FICT', 'LRL STACKS', 'LRL YA-FICT', 'NRL STACKS', 'VETMED OVERSIZE', 'VETMED STACKS']
-  }
+  },
+  'US-NCU': {
+    name: 'University of North Carolina at Chapel Hill',
+    idField: '907a',
+    tag: '99991',
+    subs: { a: 'b,c', b: 'p', c: 'h', d: 'd', x: 'r', y: 'i' },
+    lendLocs: []
+  },
 };
 
 function getSubs(field) {
@@ -139,8 +146,11 @@ export function transform(clusterStr) {
     f999s.push({ '999': f999 });
 
     // normalized item fields
-    if (lf) {
-      let items = recFields[lf.tag] || [];
+    if (lf && lf.tag) {
+      let tag = lf.tag.substring(0, 3);
+      let ind1 = lf.tag.substring(3, 4) || '';
+      let ind2 = lf.tag.substring(4, 5) || '';
+      let items = recFields[tag] || [];
       let linkedFields = {};
       if (lf.linkedField) {
         let extra = recFields[lf.linkedField] || [];
@@ -152,86 +162,95 @@ export function transform(clusterStr) {
         }
       }
       for (let i = 0; i < items.length; i++) {
+        let isItem = true;
         let item = items[i];
-        let outItem = {
-          '999' : {
-            ind1: 1,
-            ind2: 1,
-            subfields: [
-              { l: controlNumber },
-              { s: 'ISIL:' + sid },
-              { i: lf.name },
-              { t: mtype }
-            ]
-          }
+        if (ind1 && ind1 !== item.ind1) {
+          isItem = false;
+        } else if (ind2 && ind2 !== item.ind2) { 
+          isItem = false;
         }
-        let subData = getSubs(item);
-        let location = '';
-        let itype = '';
-        for (let c in lf.subs) {
-          if (lf.linkSubs && lf.linkSubs[0] && lf.subs[c].match(/^\w{3}/)) {
-            let lsf = lf.subs[c].substring(3);
-            let linkDat = subData[lf.linkSubs[0]];
-            if (linkDat) {
-              let linkedItem = linkedFields[linkDat];
-              let linkedSubs = (linkedItem) ? getSubs(linkedItem) : {};
-              let linkedData = linkedSubs[lsf] || [];
-              if (linkedData[0]) {
-                let linkedSubField = {};
-                if (c === 'a') location = linkedData[0];
-                if (c === 'x') itype = linkedData[0];
-                linkedSubField[c] = linkedData[0];
-                outItem['999'].subfields.push(linkedSubField);
+        if (isItem) {
+          let outItem = {
+            '999' : {
+              ind1: 1,
+              ind2: 1,
+              subfields: [
+                { l: controlNumber },
+                { s: 'ISIL:' + sid },
+                { i: lf.name },
+                { t: mtype }
+              ]
+            }
+          }
+          let subData = getSubs(item);
+          let location = '';
+          let itype = '';
+          for (let c in lf.subs) {
+            if (lf.linkSubs && lf.linkSubs[0] && lf.subs[c].match(/^\w{3}/)) {
+              let lsf = lf.subs[c].substring(3);
+              let linkDat = subData[lf.linkSubs[0]];
+              if (linkDat) {
+                let linkedItem = linkedFields[linkDat];
+                let linkedSubs = (linkedItem) ? getSubs(linkedItem) : {};
+                let linkedData = linkedSubs[lsf] || [];
+                if (linkedData[0]) {
+                  let linkedSubField = {};
+                  if (c === 'a') location = linkedData[0];
+                  if (c === 'x') itype = linkedData[0];
+                  linkedSubField[c] = linkedData[0];
+                  outItem['999'].subfields.push(linkedSubField);
+                }
               }
             }
-          }
-          let codes = lf.subs[c].split(',');
-          let fdata = [];
-          for (let x = 0; x < codes.length; x++) {
-            let code = codes[x];
-            if (subData[code]) {
-              let res = subData[code].join(' ');
-              fdata.push(res);
+            let codes = lf.subs[c].split(',');
+            let fdata = [];
+            for (let x = 0; x < codes.length; x++) {
+              let code = codes[x];
+              if (subData[code]) {
+                let res = subData[code].join(' ');
+                fdata.push(res);
+              }
+            }
+            let text = fdata.join(' ');
+            text = text.trim();
+            if (c === 'c' && lf.subs.c.match(/^%/)) {
+              let cnTags = lf.subs.c.split(/\|/);
+              for (let t = 0; t < cnTags.length; t++) {
+                let tag = cnTags[t].replace(/^%/, '');
+                text = bibCall[tag];
+                if (text) break;
+              }
+            }
+            if (text) {
+              let obj = {};
+              obj[c] = text;
+              outItem['999'].subfields.push(obj);
+            }
+            if (!location && c === 'a') {
+              location = text;
+            }
+            if (!itype && c === 'x') {
+              itype = text;
             }
           }
-          let text = fdata.join(' ');
-          text = text.trim();
-          if (c === 'c' && lf.subs.c.match(/^%/)) {
-            let cnTags = lf.subs.c.split(/\|/);
-            for (let t = 0; t < cnTags.length; t++) {
-              let tag = cnTags[t].replace(/^%/, '');
-              text = bibCall[tag];
-              if (text) break;
-            }
-          }
-          if (text) {
-            let obj = {};
-            obj[c] = text;
-            outItem['999'].subfields.push(obj);
-          }
-          if (!location && c === 'a') {
-            location = text;
-          }
-          if (!itype && c === 'x') {
-            itype = text;
-          }
-        }
+        
 
-        let policy = '';
-        if (lf.lendLocs && lf.lendItypes) {
-          policy = (lf.lendLocs.indexOf(location) > -1 && lf.lendItypes.indexOf(itype) > -1) ? 'LOANABLE' : 'UNLOANABLE'; 
-        } else if (lf.lendLocs && lf.notLendItypes) {
-          policy = (lf.lendLocs.indexOf(location) > -1 && lf.notLendItypes.indexOf(itype) === -1) ? 'LOANABLE' : 'UNLOANABLE';
-        } else if (lf.lendLocs) {
-          policy = (lf.lendLocs.indexOf(location) > -1) ? 'LOANABLE' : 'UNLOANABLE';
-        } else {
-          policy = 'UNLOANABLE';
+          let policy = '';
+          if (lf.lendLocs && lf.lendItypes) {
+            policy = (lf.lendLocs.indexOf(location) > -1 && lf.lendItypes.indexOf(itype) > -1) ? 'LOANABLE' : 'UNLOANABLE'; 
+          } else if (lf.lendLocs && lf.notLendItypes) {
+            policy = (lf.lendLocs.indexOf(location) > -1 && lf.notLendItypes.indexOf(itype) === -1) ? 'LOANABLE' : 'UNLOANABLE';
+          } else if (lf.lendLocs) {
+            policy = (lf.lendLocs.indexOf(location) > -1) ? 'LOANABLE' : 'UNLOANABLE';
+          } else {
+            policy = 'UNLOANABLE';
+          }
+          if (lf.lendFunc) {
+            policy = lf.lendFunc(recFields, outItem['999']) || policy;
+          }
+          outItem['999'].subfields.push({ p: policy });
+          outItems.push(outItem);
         }
-        if (lf.lendFunc) {
-          policy = lf.lendFunc(recFields, outItem['999']) || policy;
-        }
-        outItem['999'].subfields.push({ p: policy });
-        outItems.push(outItem);
       }
     }
   }
