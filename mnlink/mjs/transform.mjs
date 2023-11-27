2773b574138751f8d01e85df1462aef375cb8da4
Charles Ledvina <charles@Charless-MacBook-Pro.local>
2023-11-27 10:36:59 -0600

    Add dakota.

--- mnlink/mjs/transform.mjs
+++ mnlink/mjs/transform.mjs
@@ -20,6 +20,13 @@ const localFields = {
     lendItypes: {bk: 1, cd: 1, djmed: 1, dmed: 1, dvp: 1, dvrc3: 1, dvrc: 1, jbk: 1, jcd: 1, jdvp: 1, jdvrc3: 1, jdvrc: 1, jmed: 1, jvp: 1, jvrc3: 1, jvrc: 1, med: 1, rdvp: 1, rdvrc3: 1, rdvrc: 1, rvp: 1, rvrc3: 1, rvrc: 1, vp: 1, vrc3: 1, vrc: 1},
     lendLocs: {al: {}, an: {}, be: {}, br: {}, bu: {}, co: {}, cs: {}, cw: {}, de: {}, eb: {}, er: {}, fo: {}, ge: {}, hl: {}, hq: {}, ki: {}, lf: {}, lp: {}, me: {}, mo: {}, pa: {}, ri: {}, ry: {}, sk: {}, sm: {}, sp: {}, sw: {}, up: {}, wp: {}}
   },
+  'US-MNWSPD': {
+    name: 'Dakota County Library',
+    ils: 'Symphony',
+    tag: '999',
+    subs: { a: 'l', c: 'a', d: 'w', b: 'i', x: 't' },
+    lendLocs: {ABE:{}, BLURAY:{}, BR:{}, BRN:{}, CD:{}, CDF:{}, CDM:{}, CDN:{}, CDNI:{}, CDSF:{}, CDW:{}, CDWLSPA:{}, DVD:{}, DVDN:{}, DVDNI:{}, EZ:{}, EZR:{}, F:{}, GF:{}, GNF:{}, JBD:{}, JBLURAY:{}, JCD:{}, JDVD:{}, JDVDN:{}, JDVDNI:{}, JF:{}, JG:{}, JGAME:{}, JGNF:{}, JH:{}, JNEW:{}, JNF:{}, JPER:{}, JPLA:{}, JPS:{}, JRDF:{}, JRDN:{}, JSC:{}, JWL:{}, JWLARABIC:{}, JWLRUS:{}, JWLSOMALI:{}, JWLSPA:{}, JWLVIE:{}, LPF:{}, LPM:{}, LPN:{}, LPSF:{}, LPW:{}, M:{}, NF:{}, PBF:{}, PBM:{}, PBR:{}, PBSF:{}, PBW:{}, PER:{}, PLA:{}, SF:{}, UF:{}, W:{}, WL:{}, WLRUS:{}, WLSOM:{}, WLSPA:{}, WLVIE:{}, YAB:{}, YACDF:{}, YACDN:{}, YAF:{}, YAGAME:{}, YAGF:{}, YAGNF:{}, YAM:{}, YANF:{}, YANI:{}, YAPLA:{}, YASPA:{}}
+  },
   'US-MNLEW': {
     name: 'Washington County Library',
     ils: 'Symphony',
