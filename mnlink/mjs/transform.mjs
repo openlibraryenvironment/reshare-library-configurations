7a809dd8ebbfcd376b42e80e85da66f01ca9e7e3
Charles Ledvina <charles@Charless-MacBook-Pro.local>
2024-03-21 11:02:18 -0500

    Add MN Historical.

--- mnlink/mjs/transform.mjs
+++ mnlink/mjs/transform.mjs
@@ -11,6 +11,13 @@ const localFields = {
     subs: { a: 'c', c: 'o', d: '2', b: 'p', x: 'y', y: 'i' },
     notLendLocs: {'BRD-BK':{}, BARNKIT:{}, BINDERY:{}, BOOKCLUB:{}, CATALOGING:{}, CHILDSTORAGE:{}, GAMEBRD:{}, GAMRES:{}, ILLN:{}, JPUZZLE:{}, MLCLLIVE:{}, MNREF:{}, MP3:{}, OFFICE:{}, ONORD:{}, OUTKIT:{}, OUTSTOR:{}, PUP:{}, PUZ:{}, REFERENCE:{}, STAFF:{}, STEAM:{}, STORAGE:{}, STORPBK:{}, VERTFILE:{}, VGAME:{}, WOW:{}}
   },
+  'US-MNHI': {
+    name: 'Minnesota Historical Society',
+    ils: 'Alma',
+    tag: '909',
+    subs: { a: 'l', c: '%092|%082|%090|%050', b: 'b', k: 'p', n: 'e', u: 'c', x: 'm', y: 'a' },
+    notLendLocs: {}
+  },
   'US-MNHW': {
     name: 'Minnesota Department of Transportation',
     ils: 'Alma',
