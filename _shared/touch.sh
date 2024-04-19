88c8b4dcc4731cf42b11fa309e2432fa40e0174a
Charles Ledvina <charles@Charless-MacBook-Pro.local>
2024-04-19 13:11:59 -0500

    Read sourceIds from a file.

--- _shared/touch.sh
+++ _shared/touch.sh
@@ -4,15 +4,20 @@ TMP='./.okapi'
 OKAPI=`cat ${TMP}/url`
 TOKEN=`cat ${TMP}/token`
 
-SRCID=$1;
-if [ -z $SRCID ]
-  then
-    echo "Usage: $0 <sourceId>"
+if [ -z $1 ]; then
+    echo "Usage: $0 [ -f read_sourceIds_from_file ] <sourceId>"
     exit
 fi
 
-URL="${OKAPI}/reservoir/clusters/touch?query=matchkeyId==goldrush%20AND%20sourceId==${SRCID}"
-echo "POST $URL"
+if [ $1 == '-f' ]; then
+  SRCIDS=`sort -u $2`
+else
+  SRCIDS=$1
+fi
 
-curl -w '\n' --http1.1 $URL -H 'content-type: application/json' -H "x-okapi-token: ${TOKEN}" -d {}
+for SRCID in $SRCIDS; do
+  URL="${OKAPI}/reservoir/clusters/touch?query=matchkeyId==goldrush%20AND%20sourceId==${SRCID}"
+  echo "POST $URL"
+  curl -w '\n' --http1.1 $URL -H 'content-type: application/json' -H "x-okapi-token: ${TOKEN}" -d {}
+done
 
