d6faf9cae419b6d500b37f1912a748672937478b
Charles Ledvina <charles@Charless-MacBook-Pro.local>
2024-08-23 11:21:19 -0500

    Add query string parameter.

--- _shared/get-clusters-source.sh
+++ _shared/get-clusters-source.sh
@@ -5,13 +5,21 @@ OKAPI=`cat ${TMP}/url`
 TOKEN=`cat ${TMP}/token | sed 's/.$//'`
 
 SRCID=$1
+PARA=$2
 
 if [ -z $SRCID ]
 then
-  echo "Usage: ${0} <sourceId>"
+  echo "Usage: ${0} <sourceId> [<additional_http_query_string>]"
   exit
 fi
 
-URL="${OKAPI}/reservoir/clusters?matchkeyid=goldrush&query=sourceId==${SRCID}&limit=100"
+if [ $PARA ]
+then
+  PARA="&${PARA}"
+fi
+
+
+URL="${OKAPI}/reservoir/clusters?matchkeyid=goldrush&query=sourceId==${SRCID}${PARA}"
+echo "GET ${URL}" 1>&2
 
 curl --http1.1 -w '\n' -s $URL -H "x-okapi-token: ${TOKEN}" | jq .
