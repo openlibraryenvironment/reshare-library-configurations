154be2397424d81ebeecc8e11f90796179d89b19
David Crossley <david@indexdata.com>
2025-08-18 16:27:23 +1000

    Add basic transformer for cclp DEVOPS-6343
    - Based on cardinal, no handlers yet

---
+++ cclp/mjs/transform.mjs
@@ -0,0 +1,273 @@
+/*
+  Subfield guide: a: location, b: barcode, c: callNumber, d: callNumberType, g: copy, k: numberOfPieces, i: institutionName, 
+  n: enumeration, u: chronology, v: volume, w: yearCaption, x: itemMaterialType, y: itemId
+*/
+
+const localFields = {
+};
+
+function getSubs(field) {
+  let subs = {};
+  field.subfields.forEach(s => {
+    let code = Object.keys(s);
+    if (!subs[code]) subs[code] = [];
+    if (subs[code].indexOf(s[code]) === -1) subs[code].push(s[code]);
+  });
+  return subs;
+}
+
+export function showSettings() {
+  return (localFields);
+}
+
+export function cluster_transform(clusterStr) {
+  let now = new Date().toISOString();
+  now = now.replace(/^(\d{4})-(\d\d)-(\d\d).*/, '$1$2$3000000.0')
+  const f005 = now;
+  let cluster = JSON.parse(clusterStr);
+  let crecs = cluster.records;
+  const f001 = cluster.clusterId;
+  let f999s = [];
+  let outItems = [];
+  let preSize = '';
+  let mainBib;
+  let isMainBib;
+  let elinks = [];
+  let linkSeen = {};
+  for (let x = 0; x < crecs.length; x++) {
+    let crec = crecs[x];
+    let sid = crec.sourceId;
+    let lid = crec.localId;
+    let rec = crec.payload.marc;
+    let unknown = false;
+    if (!rec.leader) {
+      rec.leader = '00000cam a2200229Ia 4500';
+      unknown = true
+    }
+    let rsize = rec.leader.substring(0, 5);
+    if (rsize > preSize) {
+      isMainBib = 1;
+      mainBib = { leader: rec.leader, fields: [] };
+    } else {
+      isMainBib = 0;
+    }
+    preSize = rsize;
+
+    // figure out mtypes from leader codes
+    let mt = rec.leader.substring(6, 7);
+    let bl = rec.leader.substring(7, 8);
+    let mtype = '';
+    if (unknown) {
+      mtype = 'UNK'
+    } else if (bl.match(/[sb]/)) {
+      mtype = 'CNR';
+    } else if (mt.match(/[at]/)) {
+      mtype = 'BKS';
+    } else if (mt === 'm') {
+      mtype = 'COM';
+    } else if (mt.match(/[ef]/)) {
+      mtype = 'MAP';
+    } else if (mt.match(/[cd]/)) {
+      mtype = 'SCO';
+    } else if (mt.match(/[if]/)) {
+      mtype = 'REC';
+    } else if (mt === 'g') {
+      mtype = 'VIS';
+    } else {
+      mtype = 'MIX';
+    }
+
+    let recFields = {};
+    let bibCall = {};
+
+    for (let y = 0; y < rec.fields.length; y++) {
+      let field = rec.fields[y];
+      let tag = Object.keys(field)[0];
+      if (tag.match(/050|082|090|092|099/)) {
+        let csubs = getSubs(field[tag]);
+        bibCall[tag] = (csubs.a && csubs.b) ? csubs.a[0] + ' ' + csubs.b[0] : (csubs.a) ? csubs.a[0] : '';
+      }
+      if (!recFields[tag]) recFields[tag] = [];
+      recFields[tag].push(field[tag]);
+      if (isMainBib) { 
+        if ((tag > '009' && tag < '831') || tag.match(/^88./)) {
+          mainBib.fields.push(field);
+        }
+        if (tag === '008') mainBib.fields.push(field);
+      }
+    }
+
+    let lf = localFields[sid];
+    let controlNumber = (recFields['001']) ? recFields['001'][0] : lid;
+
+    // control number updates 
+    controlNumber = controlNumber.replace(/^oai.+[\/:]/, '');
+
+    // 856 field field processing
+    let f856 = recFields['856'];
+    if (lf && lf.show856 && f856) {
+      for (let x = 0; x < f856.length; x++) {
+        let f = f856[x];
+        let s = getSubs(f);
+        if (s.u) {
+          if (!linkSeen[s.u]) {
+            elinks.push({ '856': f });
+            linkSeen[s.u] = 1;
+          };
+        }
+      }
+    }
+
+    let isSuppressed = false;
+    if (lf && lf.ils && lf.ils === 'FOLIO') {
+      let ff999 = recFields['999'];
+      if (ff999) {
+        for (let x = 0; x < ff999.length; x++) {
+          let f = ff999[x];
+          if (f.ind1 === 'f' && f.ind2 === 'f') {
+            let subs = getSubs(f);
+            if (subs.t && subs.t[0] === '1') {
+              isSuppressed = true
+            }
+          }
+        }
+      }
+    }
+
+    if (lf && lf.idField) {
+      let tag = lf.idField.substring(0, 3);
+      let sf = lf.idField.substring(3);
+      let field = recFields[tag];
+      if (lf.idField.match(/^00/)) {
+        controlNumber = (recFields[lf.idField]) ? recFields[lf.idField][0] : '';
+      } else if (field && sf) {
+        let subs = getSubs(field[0]);
+        controlNumber = (subs[sf]) ? subs[sf][0] : '';
+      }
+    }
+    let f999 = {
+      ind1: '1',
+      ind2: '0',
+      subfields: [
+        { i: cluster.clusterId },
+        { l: controlNumber },
+        { s: sid }
+      ]
+    };
+    
+    for (let a = 0; a < cluster.matchValues.length; a++) {
+      f999.subfields.push({ m: cluster.matchValues[a] });
+    }
+    if (!isSuppressed) f999s.push({ '999': f999 });
+
+    // normalized item fields
+    if (lf) {
+      let items = recFields[lf.tag] || [];
+      let linkedFields = {};
+      if (lf.linkedField) {
+        let extra = recFields[lf.linkedField] || [];
+        for (let e = 0; e < extra.length; e++) {
+          let exField = extra[e];
+          let esubs = getSubs(exField);
+          let link = esubs[lf.linkSubs[1]];
+          linkedFields[link] = exField;
+        }
+      }
+      controlNumber = controlNumber.trim();
+      for (let i = 0; i < items.length; i++) {
+        if (isSuppressed) break;
+        let item = items[i];
+        let outItem = {
+          '999' : {
+            ind1: 1,
+            ind2: 1,
+            subfields: [
+              { l: controlNumber },
+              { s: 'ISIL:' + sid },
+              { i: lf.name },
+              { t: mtype }
+            ]
+          }
+        }
+        let subData = getSubs(item);
+        let location = '';
+        let itype = '';
+        for (let c in lf.subs) {
+          if (lf.linkSubs && lf.linkSubs[0] && lf.subs[c].match(/^\w{3}/)) {
+            let lsf = lf.subs[c].substring(3);
+            let linkDat = subData[lf.linkSubs[0]];
+            if (linkDat) {
+              let linkedItem = linkedFields[linkDat];
+              let linkedSubs = (linkedItem) ? getSubs(linkedItem) : {};
+              let linkedData = linkedSubs[lsf] || [];
+              if (linkedData[0]) {
+                let linkedSubField = {};
+                if (c === 'a') location = linkedData[0];
+                if (c === 'x') itype = linkedData[0];
+                linkedSubField[c] = linkedData[0];
+                outItem['999'].subfields.push(linkedSubField);
+              }
+            }
+          }
+          let codes = lf.subs[c].split(',');
+          let fdata = [];
+          for (let x = 0; x < codes.length; x++) {
+            let code = codes[x];
+            if (subData[code]) {
+              let res = subData[code].join(' ');
+              fdata.push(res);
+            }
+          }
+          let text = fdata.join(' ');
+          text = text.trim();
+          if (c === 'c' && lf.subs.c.match(/^%/)) {
+            let cnTags = lf.subs.c.split(/\|/);
+            for (let t = 0; t < cnTags.length; t++) {
+              let tag = cnTags[t].replace(/^%/, '');
+              text = bibCall[tag];
+              if (text) break;
+            }
+          }
+          if (text) {
+            let obj = {};
+            obj[c] = text;
+            outItem['999'].subfields.push(obj);
+          }
+          if (!location && c === 'a') {
+            location = text;
+          }
+          if (!itype && c === 'x') {
+            itype = text;
+          }
+        }
+
+        let pol = 0;
+        if (lf.lendLocs && lf.lendLocs[location] || lf.notLendLocs && !lf.notLendLocs[location])  {
+          pol = 1;
+          if (lf.lendItypes && !lf.lendItypes[itype]) {
+            pol = 0;
+          }
+        }
+        if (!(lf.lendLocs || lf.notLendLocs) && lf.lendItypes && lf.lendItypes[itype]) {
+          pol = 1;
+        }
+
+        if (lf.lendFunc) {
+          pol = lf.lendFunc(recFields, outItem['999']);
+        }
+        let policy = (pol) ? 'LOANABLE' : 'UNLOANABLE';
+        outItem['999'].subfields.push({ p: policy });
+        outItems.push(outItem);
+      }
+    }
+  }
+
+  mainBib.fields.unshift({ '005': f005 });
+  mainBib.fields.unshift({ '001': f001 });
+  if (elinks && elinks[0]) mainBib.fields.push(...elinks);
+  mainBib.fields.push(...f999s);
+  mainBib.fields.push(...outItems);
+  // return '{}'
+  return JSON.stringify(mainBib, null, 2);
+}
+
