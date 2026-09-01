fb65f8f2b720ad1b062d033d11891af28add7bd1
Charles Ledvina <charles@Charless-MacBook-Pro.local>
2026-09-01 14:57:21 -0500

    Make all US-MBAT unloanable

--- flo/mjs/transform.mjs
+++ flo/mjs/transform.mjs
@@ -10,8 +10,8 @@ const localFields = {
     show856: true,
     tag: '952',
     subs: { a: 'd', b: 'm', c: 'e', d: 'h', k: 'n', n: 'k', u: 'l', v: 'j', x: 'p' },
-    lendItypes: { 'Can circulate': 1, 'Standard': 1 },
-    lendLocs: {'At Bindery':{}, 'Atlas (LC)':{}, 'Children\'s Library':{}, 'Children\'s Picture Books':{}, 'Cutter Classification':{}, 'Featured Books':{}, 'Library of Congress Classification Map':{}, 'Library of Congress Classification':{}, 'New Book (14 Days)':{}, 'New Book (28 Days)':{}, 'New Books Shelves':{}, 'Off-Site Storage (Please Consult Reference Librarian)':{}, 'Off-Site Storage Boxes (Please Consult Reference Librarian)':{}, 'Young Adult Collection':{}}
+    lendItypesx: { 'Can circulate': 1, 'Standard': 1 },
+    lendLocsx: {'At Bindery':{}, 'Atlas (LC)':{}, 'Children\'s Library':{}, 'Children\'s Picture Books':{}, 'Cutter Classification':{}, 'Featured Books':{}, 'Library of Congress Classification Map':{}, 'Library of Congress Classification':{}, 'New Book (14 Days)':{}, 'New Book (28 Days)':{}, 'New Books Shelves':{}, 'Off-Site Storage (Please Consult Reference Librarian)':{}, 'Off-Site Storage Boxes (Please Consult Reference Librarian)':{}, 'Young Adult Collection':{}}
   },
   'US-MBMSA': {
     name: 'MassArt',
